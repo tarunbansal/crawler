a29bb3eae14f66fc72031404757db7c2a1cf71c8
root <tarunbansal.me@gmail.com>
2015-05-19 00:33:43 +0530

    added bs4 source

--- LinkCrawler.py
+++ LinkCrawler.py
@@ -9,6 +9,7 @@ import traceback
 from collections import deque
 
 VISITED_URLS = {}
+CRAWLED_URLS = {}
 CRAWL_BUFFER = deque([])
 CRAWLER_DEFAULT_WORKERS = 10
 WORKER_WAIT_INTERVAL = 1 #seconds
@@ -21,6 +22,8 @@ class WorkerThread(threading.Thread):
         self.name = name
         
     def run(self):
+        '''Start function for each thread'''
+        
         while not self.__crawler.kill and self.is_alive():
             try:
                 if len(CRAWL_BUFFER) > 0:
@@ -35,7 +38,6 @@ class WorkerThread(threading.Thread):
                 print("Unknown exception occured while doing worker task" + str(e))
                 traceback.print_exc()
         print("Stopping Worker : " + str(self.name))
-        print("List of crawled URL's is : \n " + str(VISITED_URLS))
 
 class URL():
     def __init__(self,strURL):
@@ -46,6 +48,8 @@ class URL():
         self.validateURL()
     
     def validateURL(self):
+        '''Validate the URL's scheme and netloc'''
+        
         pURL = urlparse(self.url)
         if pURL.netloc:
             self.netloc = pURL.netloc
@@ -57,6 +61,7 @@ class URL():
             self.valid = False
 
 class WebCrawler():
+    _lock = threading.Lock()
     kill = False
     count = 0
     listworkers = []
@@ -65,6 +70,8 @@ class WebCrawler():
         self.__startWorkers()
 
     def __startWorkers(self):
+        '''Start the worker threads'''
+        
         try:
             for workerIndex in range(CRAWLER_DEFAULT_WORKERS):
                 strWorkerName = "Worker " + str(workerIndex)
@@ -78,19 +85,17 @@ class WebCrawler():
             exit()
 
     def crawl(self,urlObj):
-        #BASE_URL = CRAWL_BUFFER.popleft()
+        '''Main function to crawl URL's '''
+        
         try:
-            if ((urlObj.valid) and (urlObj.url not in VISITED_URLS.keys())):
-                print("About to open url : " + str(urlObj.url))
+            if ((urlObj.valid) and (urlObj.url not in CRAWLED_URLS.keys())):
                 rsp = urlcon.urlopen(urlObj.url,timeout=2)
                 hCode = rsp.read()
                 soup = BeautifulSoup(hCode)
-                #print(soup)
                 links = self.scrap(soup)
-                self.count += 1
                 boolStatus = self.checkmax()
                 if boolStatus:
-                    VISITED_URLS.setdefault(urlObj.url,"True")
+                    CRAWLED_URLS.setdefault(urlObj.url,"True")
                 else:
                     return
                 for eachLink in links:
@@ -102,16 +107,21 @@ class WebCrawler():
                         if not parsedURL.scheme and not parsedURL.netloc:
                             print("No scheme and host found for "  + str(eachLink))
                             newURL = urlunparse(parsedURL._replace(**{"scheme":urlObj.scheme,"netloc":urlObj.netloc}))
-                            #print("New URL is "  + str(newURL))
                             eachLink = newURL
                         elif not parsedURL.scheme :
                             print("Scheme not found for " + str(eachLink))
                             newURL = urlunparse(parsedURL._replace(**{"scheme":urlObj.scheme}))
-                            #print("New URL is "  + str(newURL))
                             eachLink = newURL
-                            #print("Scheme and host are found for "  + str(eachLink))
                         print(" Found child link " + eachLink)
                         CRAWL_BUFFER.append(eachLink)
+                        with self._lock:
+                            self.count += 1
+                            print(" Count is =================> " + str(self.count))
+                        boolStatus = self.checkmax()
+                        if boolStatus:
+                            VISITED_URLS.setdefault(eachLink, "True")
+                        else:
+                            return
             else:
                 print("Invalid URL or URL present in visited " + str(urlObj.url))
         except Exception as e:
@@ -119,12 +129,16 @@ class WebCrawler():
             traceback.print_exc()
 
     def scrap(self,soup):
+        '''Scrap all links from the soup object'''
+        
         rec_links = []
         for link in soup.find_all('a'):
             rec_links.append(link.get('href'))
         return rec_links
     
     def checkmax(self):
+        '''Check if Upper limit on URL's to be scrapped is reached'''
+        
         boolStatus = True
         if MAX_COUNT_LIMIT and self.count >= MAX_COUNT_LIMIT:
             print(" Maximum count reached. Now exiting and stopping workers :( ")
@@ -132,36 +146,28 @@ class WebCrawler():
             boolStatus = False
         return boolStatus
 
+def saveDataToFile(listData):
+    '''Save output data in a file under current directory'''
+    
+    pass
+
 if __name__ == '__main__':
-    #getHTMLCode(str(sys.argv[1]))
     try:
         if len(sys.argv) == 3:
             MAX_COUNT_LIMIT = int(sys.argv[2])
         CRAWL_BUFFER.append(str(sys.argv[1]))
+        VISITED_URLS.setdefault(str(sys.argv[1]), "True")
         webC = WebCrawler()
         while webC.listworkers[0].is_alive():
             webC.listworkers[0].join(1)
     except KeyboardInterrupt:
-        print("**************** KBI *******************")
-        webC.kill = True
+        print("**************** Keyboard interrupt occured. Stopping all threads *******************")
         exit(0)
     except Exception as e:
         print("Unknown exception occured in main" + str(e))
         traceback.print_exc()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    finally:
+        print("Number of URL's scrapped : " + str(len(VISITED_URLS)))
+        print("================ VISITED URL's ==========================\n"+ str(VISITED_URLS) + "\n=======================================")
+        saveDataToFile(VISITED_URLS.keys())
+        webC.kill = True
