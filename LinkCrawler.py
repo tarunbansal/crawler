21d6bfd9c0b9ff3190004b63132e6698e59d7e9e
root <tarunbansal.me@gmail.com>
2015-05-19 12:59:46 +0530

    Final Changes

--- LinkCrawler.py
+++ LinkCrawler.py
@@ -5,6 +5,7 @@ from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 import sys
 import socket
+import os, codecs
 import time
 import threading
 import traceback
@@ -13,7 +14,7 @@ from collections import deque
 VISITED_URLS = {}
 CRAWLED_URLS = {}
 CRAWL_BUFFER = deque([])
-CRAWLER_DEFAULT_WORKERS = 5
+CRAWLER_DEFAULT_WORKERS = 4
 WORKER_WAIT_INTERVAL = 1 #seconds
 MAX_COUNT_LIMIT = None
 
@@ -31,15 +32,16 @@ class WorkerThread(threading.Thread):
                 if len(CRAWL_BUFFER) > 0:
                     strURL = CRAWL_BUFFER.popleft()
                     urlObj = URL(strURL)
-                    print("URL " + str(urlObj.url) + " about to be crawled by Worker : " + str(self.name))
+                    #print("URL " + str(urlObj.url) + " about to be crawled by Worker : " + str(self.name))
                     self.__crawler.crawl(urlObj)
                 else:
-                    print("No work for worker :" + str(self.name))
+                    pass
+                    #print("No work for worker :" + str(self.name))
                 time.sleep(WORKER_WAIT_INTERVAL)
             except Exception as e:
                 print("Unknown exception occured while doing worker task" + str(e))
                 traceback.print_exc()
-        print("Stopping Worker : " + str(self.name))
+        #print("Stopping Worker : " + str(self.name))
 
 class URL():
     def __init__(self,strURL):
@@ -104,28 +106,31 @@ class WebCrawler():
                     if eachLink not in VISITED_URLS:
                         parsedURL = urlparse(eachLink)
                         if parsedURL.scheme and "javascript" in parsedURL.scheme:
-                            print("***************Javascript found in scheme " + str(eachLink) + "**************")
+                            #print("***************Javascript found in scheme " + str(eachLink) + "**************")
                             continue
                         '''Handle internal URLs '''
-                        if not parsedURL.scheme and not parsedURL.netloc:
-                            print("No scheme and host found for "  + str(eachLink))
-                            newURL = urlunparse(parsedURL._replace(**{"scheme":urlObj.scheme,"netloc":urlObj.netloc}))
-                            eachLink = newURL
-                        elif not parsedURL.scheme :
-                            print("Scheme not found for " + str(eachLink))
-                            newURL = urlunparse(parsedURL._replace(**{"scheme":urlObj.scheme}))
-                            eachLink = newURL
-                        if eachLink not in VISITED_URLS: #Check again for internal URL's
-                            print(" Found child link " + eachLink)
-                            CRAWL_BUFFER.append(eachLink)
-                            with self._lock:
-                                self.count += 1
-                                print(" Count is =================> " + str(self.count))
-                            boolStatus = self.checkmax()
-                            if boolStatus:
-                                VISITED_URLS.setdefault(eachLink, "True")
-                            else:
-                                return
+                        try:
+                            if not parsedURL.scheme and not parsedURL.netloc:
+                                #print("No scheme and host found for "  + str(eachLink))
+                                newURL = urlunparse(parsedURL._replace(**{"scheme":urlObj.scheme,"netloc":urlObj.netloc}))
+                                eachLink = newURL
+                            elif not parsedURL.scheme :
+                                #print("Scheme not found for " + str(eachLink))
+                                newURL = urlunparse(parsedURL._replace(**{"scheme":urlObj.scheme}))
+                                eachLink = newURL
+                            if eachLink not in VISITED_URLS: #Check again for internal URL's
+                                #print(" Found child link " + eachLink)
+                                CRAWL_BUFFER.append(eachLink)
+                                with self._lock:
+                                    self.count += 1
+                                    #print(" Count is =================> " + str(self.count))
+                                boolStatus = self.checkmax()
+                                if boolStatus:
+                                    VISITED_URLS.setdefault(eachLink, "True")
+                                else:
+                                    return
+                        except TypeError:
+                            print("Type error occured ")
             else:
                 print("URL already present in visited " + str(urlObj.url))
         except socket.timeout as e:
@@ -158,14 +163,28 @@ class WebCrawler():
         
         boolStatus = True
         if MAX_COUNT_LIMIT and self.count >= MAX_COUNT_LIMIT:
-            print(" Maximum count reached. Now exiting and stopping workers :( ")
+            #print(" Maximum count reached. Now exiting and stopping workers :( ")
             self.kill = True
             boolStatus = False
         return boolStatus
 
 def saveDataToFile(listData):
     '''Save output data in a file under current directory'''
-    pass
+    
+    boolToReturn = True
+    fileName = None
+    try:
+        path = os.getcwd()
+        fileName = path + "/" +"output.txt"
+        file_obj = codecs.open(fileName,'w')
+        for eachLink in listData:
+            file_obj.write(str(eachLink) + "\n")
+    except Exception as e:
+        boolToReturn = False
+        print("Unknown exception occured in saving data to file " + str(e))
+        traceback.print_exc()
+    finally:
+        return boolToReturn, fileName
 
 if __name__ == '__main__':
     try:
@@ -183,7 +202,10 @@ if __name__ == '__main__':
         print("Unknown exception occured in main" + str(e))
         traceback.print_exc()
     finally:
-        print("Number of URL's scrapped : " + str(len(VISITED_URLS)))
-        print("================ VISITED URL's ==========================\n"+ str(VISITED_URLS) + "\n=======================================")
-        saveDataToFile(VISITED_URLS.keys())
+        boolStatus,fileName = saveDataToFile(VISITED_URLS.keys())
+        if boolStatus:
+            print("Links saved in the repository " + str(fileName) + " successfully")
+            print("Number of links : "  + str(len(VISITED_URLS)))
+        else:
+            print("Unable to save links " + str(VISITED_URLS))
         webC.kill = True
